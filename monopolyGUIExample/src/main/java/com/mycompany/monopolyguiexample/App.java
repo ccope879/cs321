f60b1db0ab32bb54b7122d5427bee38ce1f0aac0
Julio <73224592+JulioERS@users.noreply.github.com>
2022-11-11 16:37:00 -0600

    Updated App.java and SceneController

--- monopolyGUIExample/src/main/java/com/mycompany/monopolyguiexample/App.java
+++ monopolyGUIExample/src/main/java/com/mycompany/monopolyguiexample/App.java
@@ -8,7 +8,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
@@ -31,12 +30,31 @@ public class App extends Application {
             // Failed to load fxml
         }
     }
-    
+
+   /* public static Scene scene;
+    AnchorPane root = FXMLLoader.load(getClass().getResource("Board.fxml"));
+    //String resource = "Board.fxml";
+    //FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("Board.fxml"));
+    //static AnchorPane root = loader.load();
+
+    public App() throws IOException {
+    }
+    */
     @Override
     public void start(Stage stage) throws IOException {
-        
-        AnchorPane root = FXMLLoader.load(getClass().getResource("Board.fxml"));
-        
+        /*Rectangle rect = new Rectangle();  // import the built rectangle class for Javafx app..
+                                                                         // Set height, Width and X and Y axis.
+        rect.setX(200);
+        rect.setY(200);
+        rect.setWidth(100);
+        rect.setHeight(100);
+        rect.setStroke(Color.BLACK);
+        rect.setFill(Color.GREEN);
+
+        root.getChildren().add(rect);*/
+        // Creates parent root for initial launch of application
+        Parent root;
+        root = FXMLLoader.load(getClass().getResource("/fxml/startScreen.fxml"));     
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
@@ -47,12 +65,21 @@ public class App extends Application {
         //scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
+    //private static Parent loadFXML(String fxml) throws IOException {
+        //FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        //return fxmlLoader.load();
+    //}
     
     public static void main(String[] args) {
         launch();
     }
+    
+    /*public AnchorPane getRoot() {
+        return root;
+    }*/
+/*
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }*/
 }
