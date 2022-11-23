07d011a0e0fd255112d9fc5399bc7d2fa3097b07
ccope879 <83920786+ccope879@users.noreply.github.com>
2022-11-22 18:52:21 -0600

    Update Space.java

--- monopolyGUIExample/src/main/java/com/mycompany/monopolyguiexample/Space.java
+++ monopolyGUIExample/src/main/java/com/mycompany/monopolyguiexample/Space.java
@@ -5,7 +5,13 @@
 package com.mycompany.monopolyguiexample;
 
 import java.util.ArrayList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  *
@@ -14,13 +20,31 @@ import javafx.scene.control.Button;
 public class Space {
     String name;
     int position;
+    //private ActionEvent event;
+ 
 
     Space(String n, int p) {
         this.name = n;
         this.position = p;
     }
 
-    void handleEvent(int diceRoll, ArrayList<Player> all_players, Player player, Button roll_button, Button move_button, Button yes, Button no) {
-
+    void handleEvent(int diceRoll, ArrayList<Player> allPlayers, Player player, 
+            Button rollButton, Button moveButton, Button yes, Button no, 
+            Button endTurn, Button viewEvent) {
+        endTurn.setDisable(false);
+        
+        
     }
+    
+    void spawnPopUp(Text popupDescription, int diceRoll, ArrayList<Player> allPlayers, 
+        Player currentPlayer) {
+        
+    }
+    
+    /*void spawnPopUp() {
+        Stage dialog = new Stage();
+        dialog.initOwner(parentStage);
+        dialog.initModality(Modality.APPLICATION_MODAL); 
+        dialog.showAndWait();
+    }*/
 }
